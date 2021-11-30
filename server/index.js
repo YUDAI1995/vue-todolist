3d7a56179314982916a485fd6e575aeb09d7bfd7
yudaiy <y.yamamoto991103@gmail.com>
2021-12-01 00:48:51 +0900

    update route process

--- server/index.js
+++ server/index.js
@@ -2,30 +2,22 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const path = require('path');
+var path = require('path');
 const io = require('socket.io').listen(server);
 
-//クライアントからリクエストあった時の処理
-// server.on('request', (req, res) => {
-//    const stream = fs.createReadStream('index.html');
-//   const stream = fs.createReadStream('/');
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   stream.pipe(res);
-// });
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, '../dist/index.html')));
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
-//app.use(express.static(path.join(__dirname, "../client/build")));
 
 //Socket.io
 io.on('connection', (socket) => {
-  socket.emit('hello', { hello: 'world' });
   socket.on('event', (data) => {
     console.log(data);
   });
 });
 
-// socketイベントの設定
 io.on('connection', (socket) => {
   console.log('connected:', socket.id);
 
@@ -34,7 +26,7 @@ io.on('connection', (socket) => {
     console.log('disconnected:', socket.id);
   });
 
-  // タスクの追加
+  // タスク追加
   socket.on('addtask', (task) => {
     console.log('added!');
     io.emit('addtask', task);
